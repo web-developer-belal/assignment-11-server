1307dbde5f70de8760679fe1a647381e9344e824
unknown <programmer.m.belal@gmail.com>
2025-06-10 12:29:10 +0600

    Create mongodb user password and coneect with database successfully

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require("dotenv").config();
 
 const port = process.env.PORT || 3000;
@@ -9,9 +10,38 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
+// Mongo connection
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.jzcyg6t.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const artifactCollections = client.db("artifacts").collection("artifacts");
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    //await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
