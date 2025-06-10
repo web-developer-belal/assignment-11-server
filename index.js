835ed6da54397c65b9484b758cec7a6c23e63959
unknown <programmer.m.belal@gmail.com>
2025-06-10 12:58:27 +0600

    Artifact add api create and tested successfuly

--- index.js
+++ index.js
@@ -33,9 +33,15 @@ async function run() {
     const artifactCollections = client.db("artifacts").collection("artifacts");
 
 
+    app.post("/artifacts", async (req, res) => {
+      const artifact = req.body;
+      const result = await artifactCollections.insertOne(artifact);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    // await client.db("admin").command({ ping: 1 });
+    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
     //await client.close();
